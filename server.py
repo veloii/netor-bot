cd4d454945b94ef4e263d2f8164b9de7bb4d16cc
zelrdev <jacob@zelr.me>
2022-06-02 09:31:50 +0100

    make port var int

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from api import api
 from dotenv import load_dotenv
 load_dotenv()
 
-PORT = os.environ["PORT"] | 80
+PORT = int(os.environ["PORT"]) | 80
 app = Quart(__name__)
 app.register_blueprint(api)
 
